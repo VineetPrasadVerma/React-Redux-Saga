87cb8a43547bfe13596011ec9c3c7b3fc9cac320
Vineet <vineet@styldod.com>
2022-11-18 18:19:28 +0530

    Add Router and showing amount and product in cart page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Main from './components/Main';
+import Cart from './components/Cart';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div >
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     const cart = useSelector(state => state.cartData)
     console.log(cart)
     return (
         <div className="header">
-            <div className="cart-div">
-                <span>{cart.length}</span>
-                <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="" />
-            </div>
+            <Link to="/"><h1 className='logo'>E-COMM</h1></Link>
+            <Link to="/cart">
+                <div className="cart-div">
+                    <span>{cart.length}</span>
+                    <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="" />
+                </div>
+            </Link>
         </div>
     )
 }
